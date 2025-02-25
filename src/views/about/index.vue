f9b7cacf9e6d04c430fce756763158c6da27f9cc
vitecent <351353562@qq.com>
2025-02-25 10:40:05 +0800

    初始化

---
+++ src/views/about/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineOptions({ name: 'AboutPage' });
+</script>
+
+<template>
+  <ElSpace direction="vertical" fill :size="16">
+    <ElCard header="header" size="small" segmented class="card-wrapper">
+      <p>About</p>
+    </ElCard>
+  </ElSpace>
+</template>
+
+<style scoped></style>
---
+++ src/views/user-center/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineOptions({ name: 'UserPage' });
+</script>
+
+<template>
+  <ElSpace direction="vertical" fill :size="16">
+    <ElCard header="header" size="small" segmented class="card-wrapper">
+      <p>User</p>
+    </ElCard>
+  </ElSpace>
+</template>
+
+<style scoped></style>
